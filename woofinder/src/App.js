6d31ee2defbb0394c767353e63350452a9694eea
Marcin Ruszczyk <marcin.ruszczyk@gazeta.pl>
2022-01-19 16:39:32 +0100

    Start page

--- woofinder/src/App.js
+++ woofinder/src/App.js
@@ -1,10 +1,14 @@
-
+import { AddWanted} from './components/addwanted'
+import {SearchForm} from './components/search-form'
+import {Top} from './components/topbar'
 
 function App() {
   return (
     <div className="App">
-
-    </div>
+      <Top>WOOFinder</Top>
+      <SearchForm></SearchForm>
+      <AddWanted></AddWanted>
+   </div>
   );
 }
 
