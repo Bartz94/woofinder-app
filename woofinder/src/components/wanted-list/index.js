59e2f9e9fb701c686db95ba926036ba1385b59c1
Bartosz Czyzewski <bart94946@gmail.com>
2022-01-23 18:57:00 +0100

    Added component skeleton

--- woofinder/src/components/wanted-list/index.js
+++ woofinder/src/components/wanted-list/index.js
@@ -1,1 +1,72 @@
 //Bartosz
+import styled from 'styled-components';
+import { Typography } from '@mui/material';
+import { Box } from '@mui/system';
+import { Avatar } from '@mui/material';
+import { Container } from '@mui/material';
+
+const InfoSection = styled.div`
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    margin-top: 50px;
+    align-items: center;
+ `;
+
+const DetailsSection = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    margin: 20px;
+ `;
+
+export const WantedList = () => {
+    return <>
+        <Typography variant='h6' sx={{ alignSelf: 'left' }}>Liczba zaginięć zwierząt:{Math.floor(Math.random() * 100)}</Typography>
+        <Container fixed maxWidth='md' sx={{ marginTop: '20px', backgroundColor: '#f2f2f2', borderRadius: '0 50px 0 0', minHeight: '30vh' }}>
+            <InfoSection style={{ padding: '10px', margin: '10px' }}>
+                <div>
+                    <Avatar src="https://picsum.photos/100/100" alt="dog" sx={{ width: '7em', height: '7em' }} />
+                </div>
+                <div>
+                    <Typography sx={{ fontSize: '1.8em', fontWeight: 'bold', margin: '' }}>Lulek</Typography>
+                </div>
+                <Box>
+                    <Typography sx={{ fontSize: '1em', fontStyle: 'italic', fontWeight: '500', paddingTop: '20px' }}>Cechy zwierzaka:</Typography>
+                    <Typography sx={{
+                        maxWidth: '100px',
+                        maxHeight: '120px',
+                        overflow: 'scroll',
+                        overflowY: 'hidden'
+                    }}>
+                        Lorem lorem lorem lorem loremLorem lorem loremLorem lorem lorem </Typography>
+                </Box>
+                <div>
+                    <Typography>Zobacz na mapie</Typography>
+                </div>
+                <div>
+                    <Typography>Warszawa</Typography>
+                </div>
+                <div>
+                    <Typography>Jeśli widziałeś zwierzaka napisz</Typography>
+                    <Typography>Pokaż więcej</Typography>
+                </div>
+            </InfoSection>
+            <DetailsSection style={{ marginBottom: '20px' }}>
+                <Box sx={{
+                    backgroundColor: '#ffff',
+                    backgroundSize: 'cover',
+                    padding: '30px',
+                    borderRadius: '25px',
+                }}>
+                    <Typography sx={{ fontSize: '1.2em', fontWeight: '500', }}>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+                        standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled
+                        it to make a type specimen book.r since the 1500s, when an unknown printer took a galley of type and scrambled
+                        it to make a type specimen book.r since the 1500s, when an unknown printer took a galley of type and scrambled
+                        it to make a type specimen book.r since the 1500s, when an unknown printer took a galley of type and scrambled
+                    </Typography>
+                </Box>
+            </DetailsSection>
+        </Container>
+    </>;
+};
--- woofinder/src/content/main-page/index.js
+++ woofinder/src/content/main-page/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { SearchForm } from '../../components/search-form';
 import { Top } from '../../components/topbar'
 import { AddWanted } from '../../components/addwanted'
+import { WantedList } from '../../components/wanted-list';
 
 const MainPage = () => {
     return <>
-        <Top></Top>
+        {/* <Top></Top>
         <SearchForm></SearchForm>
-        <AddWanted></AddWanted>
+        <AddWanted></AddWanted> */}
+        <WantedList></WantedList>
     </>;
 };
 
