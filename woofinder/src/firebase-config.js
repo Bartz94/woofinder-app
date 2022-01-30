dd67e1362abbdea3d7d6bea55813dc43434d64b7
Bartosz Czyzewski <bart94946@gmail.com>
2022-01-30 15:51:23 +0100

    fetched data from firebase

--- woofinder/src/firebase-config.js
+++ woofinder/src/firebase-config.js
@@ -1,3 +1,6 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from '@firebase/firestore'
+
 export const firebaseConfig = {
     apiKey: "AIzaSyDi-nTvrEcCGTGoLWTXo0XnWRoQJObi-bs",
     authDomain: "woofinder-53a38.firebaseapp.com",
@@ -6,3 +9,7 @@ export const firebaseConfig = {
     messagingSenderId: "582591290081",
     appId: "1:582591290081:web:c739514fb91c4e997f75a0"
 };
+
+const app = initializeApp(firebaseConfig)
+
+export const db = getFirestore(app)
