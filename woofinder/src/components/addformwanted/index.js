9b728b11bbcb2d3b99f6468967d909b4736580a1
Marcin Ruszczyk <marcin.ruszczyk@gazeta.pl>
2022-01-31 21:20:01 +0100

    Dodawanie ogłoszenia

--- woofinder/src/components/addformwanted/index.js
+++ woofinder/src/components/addformwanted/index.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import {useState} from 'react';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Dialog from '@mui/material/Dialog';
@@ -8,10 +9,14 @@ import DialogTitle from '@mui/material/DialogTitle';
 import styled from 'styled-components';
 import Avatar from '@mui/material/Avatar';
 import CancelIcon from '@mui/icons-material/Cancel';
-import Typography from "@mui/material/Typography";
-import { Filepicker } from '../filepicker'
 import PropTypes from 'prop-types';
 import CloseIcon from '@mui/icons-material/Close';
+import { getFirestore,collection, addDoc} from "https://www.gstatic.com/firebasejs/9.5.0/firebase-firestore.js";
+import { initializeApp} from "https://www.gstatic.com/firebasejs/9.5.0/firebase-app.js";
+import {firebaseConfig} from "../../firebase-config";
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore();
 
 
 const BootstrapDialog = styled(Dialog)`
@@ -25,39 +30,66 @@ height:500px;
 overlay: hidden;
 `;
 
-const Wraps = styled.div`
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-`;
 
-const Wrapperwanted1 = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin: 0.5em auto;
-  width:200px
-  
-`;
+export const AddFormWanted = () => {
 
-const Wrapperwanted2 = styled.div`
-  display: flex;
-  flex-direction: column;
-  width:350px
-`;
+  const [open, setOpen] = React.useState(false);
 
-const Wrapperwanted3 = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: end;
-  
-`;
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleChange = (e) => {
+    setFormData({
+        ...formData,
+        [e.target.name]: e.target.value
+    })
+  }
+
+  const [formData, setFormData] = useState({
+    address: '' ,
+    breed: '',
+    citylost: '',
+    local: '',
+    lost_date:'',
+    name:'',
+    owner:'',
+    phone:'',
+    photolink:'',
+    description:'',
+    details:''
+  });
+
+ const { name, address, breed, citylost,local,lost_date,phone,owner,description,details } = formData;
+
+ const handleAdd = async () => {
+      
+   await addDoc(collection(db, "Wanted"), {
+       address: address ,
+       breed: breed,
+       citylost: citylost,
+       coordinates: local,
+       dateofmissing:lost_date,
+       name:name,
+       ownername:owner,
+       phone:phone,
+       photolink:"link",
+       description:description,
+       details:details
+  });
+        setOpen(false);
+}   
 
 
 const BootstrapDialogTitle = (props) => {
   const { children, onClose, ...other } = props;
 
   return (
+
     <DialogTitle sx={{ m: 0, p: 2 }} {...other}>
       {children}
       {onClose ? (
@@ -88,28 +120,12 @@ BootstrapDialogTitle.propTypes = {
   onClose: PropTypes.func.isRequired,
 };
 
-export const AddFormWanted = () => {
-
-  const [open, setOpen] = React.useState(false);
-
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
-
   return (
     <div>
       <Button variant="contained" sx={{ color: 'black', fontSize: '16px', border: 'none ', borderRadius: '20px', backgroundColor: '#e2e2e2', textTransform: 'capitalize', fontWeight: 'bold', mb: 3 }} onClick={handleClickOpen}>
         Dodaj ogłoszenie
       </Button>
-      <BootstrapDialog
-        onClose={handleClose} maxWidth
-        aria-labelledby="customized-dialog-title"
-        open={open}
-      >
+      <BootstrapDialog  onClose={handleClose} maxWidth="1200ox" aria-labelledby="customized-dialog-title" open={open}>
         <BootstrapDialogTitle variant="h4" sx={{ mt: 1, ml: 3, fontFamily: 'Segoe UI', fontWeight: 'bold', textTransform: "uppercase" }} id="customized-dialog-title" onClose={handleClose}>
           Formularz zgłoszeniowy
         </BootstrapDialogTitle>
@@ -119,27 +135,27 @@ export const AddFormWanted = () => {
           <Button sx={{ ml: 8, mt: 2 }}>Dodaj zdjęcie</Button>
 
 
-
-          <TextField className="inputRounded" sx={{ fontSize: '0.6em', borderRadius: '25px', border: '1px solid silver', ml: 11, mt: -18 }} id="name" label="Imię psa" type="text" />
-          <TextField className="inputRounded" sx={{ fontSize: '0.6em', borderRadius: '25px', border: '1px solid silver', ml: 35, mt: -16 }} id="breed" label="Rasa" type="text" />
-          <TextField className="inputRounded" sx={{ fontSize: '0.6em', borderRadius: '25px', border: '1px solid silver', ml: 35, mt: -10, }} id="lost_date" label="" type="date" />
-          <TextField className="inputRounded" sx={{ fontSize: '0.6em', borderRadius: '25px', border: '1px solid silver', ml: 35, mt: -4 }} id="local" label="Ostatnia lokalizacja pobytu" type="text" />
-          <TextField className="inputRounded" sx={{ fontSize: '0.6em', borderRadius: '25px', border: '1px solid silver', ml: 35, mt: 2 }} id="owner" label="Imię właściciela" type="text" />
-          <TextField className="inputRounded" sx={{ fontSize: '0.6em', borderRadius: '25px', border: '1px solid silver', ml: 35, mt: 2 }} id="phone" label="Telefon właściciela" type="phone" />
-          <TextField className="inputRounded" sx={{ fontSize: '0.6em', borderRadius: '25px', border: '1px solid silver', ml: 35, mt: 2 }} id="address" label="Adres zamieszakania właściciela" type="text" />
-          <TextField className="inputRounded" sx={{ fontSize: '0.6em', borderRadius: '25px', border: '1px solid silver', ml: 35, mt: 2 }} id="description" name="description" label="opis"></TextField>
-          <TextField className="inputRounded" sx={{ fontSize: '0.6em', borderRadius: '25px', border: '1px solid silver', ml: 35, mt: 2 }} id="description" name="description" label="znaki szczególne"></TextField>
+          <TextField className="inputRounded" sx={{ fontSize: '0.6em', borderRadius: '25px', border: '1px solid silver', ml: 11, mt: -18 }} id="name" name="name" value={name} onChange={handleChange} label="Imię psa" type="text" />
+          <TextField className="inputRounded" sx={{ fontSize: '0.6em', borderRadius: '25px', border: '1px solid silver', ml: 35, mt: -16 }} id="breed" name="breed" value={breed} onChange={handleChange} label="Rasa" type="text" />
+          <TextField className="inputRounded" sx={{ fontSize: '0.6em', borderRadius: '25px', border: '1px solid silver', ml: 35, mt: -10, }} id="lost_date" name="lost_date" value={lost_date} onChange={handleChange} label="Data zaginięcia" type="date" />
+          <TextField className="inputRounded" sx={{ fontSize: '0.6em', borderRadius: '25px', border: '1px solid silver', ml: 35, mt: -4 }} id="local" name="local" value={local} onChange={handleChange} label="Ostatnia lokalizacja pobytu" type="text" />
+          <TextField className="inputRounded" sx={{ fontSize: '0.6em', borderRadius: '25px', border: '1px solid silver', ml: 35, mt: 2 }} id="owner" name="owner" value={owner} onChange={handleChange} label="Imię właściciela" type="text" />
+          <TextField className="inputRounded" sx={{ fontSize: '0.6em', borderRadius: '25px', border: '1px solid silver', ml: 35, mt: 2 }} id="phone" name="phone" value={phone} onChange={handleChange} label="Telefon właściciela" type="phone" />
+          <TextField className="inputRounded" sx={{ fontSize: '0.6em', borderRadius: '25px', border: '1px solid silver', ml: 35, mt: 2 }} id="address" name="address" value={address} onChange={handleChange} label="Adres zamieszakania właściciela" type="text" />
+          <TextField className="inputRounded" sx={{ fontSize: '0.6em', borderRadius: '25px', border: '1px solid silver', ml: 35, mt: 2 }} id="description" name="description" value={description} onChange={handleChange} label="opis"></TextField>
+          <TextField className="inputRounded" sx={{ fontSize: '0.6em', borderRadius: '25px', border: '1px solid silver', ml: 35, mt: 2 }} id="details" name="details" value={details} onChange={handleChange}  label="znaki szczególne"></TextField>
 
 
 
         </DialogContentStyle>
 
         <DialogActions>
-          <Button variant="contained" sx={{ color: 'black', fontSize: '0.8em', borderRadius: '20px', backgroundColor: '#E2E2E2', mt: -66, mr: 12, textTransform: 'capitalize', fontWeight: 'bold' }} autoFocus onClick={handleClose}>
+          <Button variant="contained" sx={{ color: 'black', fontSize: '0.8em', borderRadius: '20px', backgroundColor: '#E2E2E2', mt: -66, mr: 12, textTransform: 'capitalize', fontWeight: 'bold' }} autoFocus onClick={handleAdd}>
             Zapisz
           </Button>
+         
         </DialogActions>
       </BootstrapDialog>
     </div>
   );
-}
+ }
