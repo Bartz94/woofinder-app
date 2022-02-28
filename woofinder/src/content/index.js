58e88d6a32b7a9c65498adb254be16ee283fd8c3
Bartosz Czyzewski <bart94946@gmail.com>
2022-02-28 22:28:24 +0100

    added new route for choosen wanted

--- woofinder/src/content/index.js
+++ woofinder/src/content/index.js
@@ -1,17 +1,18 @@
 import { Routes, Route } from 'react-router-dom';
 import MainPage from './main-page';
 import WantedPage from './wanted-page';
+import { Wanted } from '../components/wanted'
 import { RegisterForm } from '../components/registerform';
 import { LoginForm } from './../components/loginform';
 import { Profile } from './profile';
-
 
 export const Content = () => (
     <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/wanted-page" element={<WantedPage />} />
+        <Route path="/wanted-page/:id" element={<Wanted />} />
         <Route path="/login" element={<LoginForm />} />
         <Route path="/register" element={<RegisterForm />} />
-        <Route path ="/profile" element={<Profile />} />
+        <Route path="/profile" element={<Profile />} />
     </Routes>
 );
